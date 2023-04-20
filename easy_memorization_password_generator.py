7b2ca4f77dea3790fc4e309b9ae1d541f10b169a
Rise <43541900+Quotion@users.noreply.github.com>
2023-04-20 08:45:32 +0300

    fix error

--- easy_memorization_password_generator.py
+++ easy_memorization_password_generator.py
@@ -5,6 +5,7 @@ from random import randint
 lenght = int(input('Введите длину пароля: '))
 letters_lib = string.ascii_letters
 digits_libr = string.digits
-password1 = [letters_lib[randint(0, len(letters_lib))] for x in range(lenght-2)]
-password2 = [digits_libr[randint(0, len(digits_libr))] for x in range(0, 2)]
-print(*password1, *password2)
+password1 = ''.join([letters_lib[randint(0, len(letters_lib) - 1)] for x in range(lenght - 2)])
+password2 = ''.join([digits_libr[randint(0, len(digits_libr) - 1)] for x in range(0, 2)])
+print(password1 + password2)
+# print(*password1, *password2)
